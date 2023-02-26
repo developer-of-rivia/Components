59e82239b2666ee09c5a8730bb210731a0f1d906
oloref <samping2money@gmail.com>
2023-02-26 19:14:45 +0700

    сделан слайдер в одну секцию

--- app/js/slider3.js
+++ app/js/slider3.js
@@ -2,9 +2,12 @@ $(document).ready(function () {
 
     // слайдер
     $('.slider__object').slick({
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
         arrow: true,
+        centerMode: true,
+        variableWidth: true,
+        //appendArrows: $('.slider__nav'),
         prevArrow: '<div class="slick-prev example__slider-prev"><svg><use xlink:href="img/svg-ui-sprite.svg#left-ungle"></use></svg></div>',
         nextArrow: '<div class="slick-next example__slider-prev"><svg><use xlink:href="img/svg-ui-sprite.svg#right-ungle"></use></svg></div>',
         responsive: [
@@ -26,3 +29,6 @@ $(document).ready(function () {
     });
 	
 });
+
+
+console.log('daa');
