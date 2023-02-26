41cf61bb839424e5fbe8cda97e2e468e66331540
oloref <samping2money@gmail.com>
2023-02-26 19:48:39 +0700

    сделано меню1

--- app/js/menu1.js
+++ app/js/menu1.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
 
-    let burgerIcon = document.querySelector('.burger-icon__bars-svg');
-    let CloseIcon = document.querySelector('.burger-icon__bars-svg');
+    let burgerIcon = document.querySelector('.burger-icon');
+    let closeIcon = document.querySelector('.menu__close');
     let menu = document.querySelector('.menu');
 
+    
     burgerIcon.addEventListener('click', function(){
         menu.classList.add('menu--open');
+    })
+    closeIcon.addEventListener('click', function(){
+        menu.classList.remove('menu--open');
     })
 	
 });
