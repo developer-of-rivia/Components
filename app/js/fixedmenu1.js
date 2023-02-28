4bd7c901bec0065fac2363df010457025122a528
oloref <samping2money@gmail.com>
2023-02-28 11:05:17 +0700

    fixed меню чутка переработано

--- app/js/fixedmenu1.js
+++ app/js/fixedmenu1.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+    let burgerIcon = document.querySelector('.burger-icon');
+    let closeIcon = document.querySelector('.menu__close');
+    let menu = document.querySelector('.menu');
+    let scrollObject = document.querySelector('.menu');
+
+    burgerIcon.addEventListener('click', function(){
+        menu.classList.add('menu--open');
+        scrollLock.disablePageScroll(scrollObject);
+    })
+    closeIcon.addEventListener('click', function(){
+        menu.classList.remove('menu--open');
+        scrollLock.enablePageScroll(scrollObject);
+    })
+
+
     window.onscroll = function showHeader() {
         var header = document.querySelector('.header');
         if(window.pageYOffset > 100){
